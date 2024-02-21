d7166d34c353b98cad8ab9c737902054199c4882
Céline <118277079+CelineIntha@users.noreply.github.com>
2024-02-21 13:26:44 +0100

    commit changement Test Unitaire

--- Calculatrice/obj/Debug/net7.0/Calculatrice.AssemblyInfo.cs
+++ Calculatrice/obj/Debug/net7.0/Calculatrice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculatrice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5275bdd030472ef59ea2262a57514cd6eb6a9a0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculatrice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculatrice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
