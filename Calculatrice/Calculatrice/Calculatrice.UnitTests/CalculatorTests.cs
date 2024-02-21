3f9d25495d771d399d8d667306890c15eec96db0
Céline <118277079+CelineIntha@users.noreply.github.com>
2024-02-21 11:25:48 +0100

    First commit

---
+++ Calculatrice/Calculatrice/Calculatrice.UnitTests/CalculatorTests.cs
@@ -0,0 +1,63 @@
+﻿using Calculatrice;
+namespace Calculatrice.UnitTests;
+
+[TestClass]
+
+public class UnitTest1
+{
+    // Test Initialize
+    private Calculator _calculator;
+
+    [TestInitialize]
+    public void Init()
+    {
+        _calculator = new Calculator();
+    }
+
+    [TestMethod]
+    // Le test va s'executer 3 fois
+    [DataRow(10, 5, 15)] // 15 = résultat attendu
+    [DataRow(999, 1, 1000)] 
+    [DataRow(1, 500, 501)] 
+    public void Addtest(int numberOne, int numberTwo, int expectedResult)
+    {;
+
+        var actualResult = _calculator.Add(numberOne, numberTwo);
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [TestMethod]
+    public void Substract_WithTwoNumbers_ReturnsSubstraction()
+    {
+        Assert.AreEqual(1, _calculator.Substract(3, 2));
+    }
+
+    [TestMethod]
+    public void Multest()
+    {
+        Assert.AreEqual(8, _calculator.Multiply(4, 2));
+    }
+
+    [TestMethod]
+    public void Divide_WithTwoStrictlyPositiveNumbers_ReturnsDivision()
+    {
+        Assert.AreEqual(2, _calculator.Divide(4, 2));
+    }
+
+    [TestMethod]
+    public void Divide_WithSecondParameterAsZero_ReturnsDivision()
+    {
+        Assert.AreEqual(2, _calculator.Divide(4, 0));
+    }
+
+    [TestMethod]
+    public void Add_WithTwoNumbers_ReturnsAddition()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(10, 5);
+
+        Assert.AreEqual(15, result);
+    }
+}
---
+++ Calculatrice/Calculatrice/Calculatrice/Calculatrice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
+
+namespace Calculatrice
+{
+    public class Calculator
+    {
+            public int Add(int numberOne, int numberTwo)
+            {
+            if (numberOne < 0 || numberTwo < 0)
+                throw new ArgumentException("Les deux paramètres ne peuvent pas être négatifs");
+
+            if (numberTwo >= 1000)
+                throw new ArgumentException("Second chiffre ne doit pas être supérieur ou égal à 1000");
+                return (numberOne + numberTwo);
+            }
+
+            public int  Substract(int numberOne, int numberTwo)
+            {
+                return (numberOne - numberTwo);
+            }
+
+            public int Divide(int numberOne, int numberTwo)
+            {
+                if (numberOne == 0 || numberTwo == 0)
+                {
+                    throw new ArgumentException("On ne peut pas diviser un nombre par 0");
+                }
+                else
+                {
+                    return (numberOne / numberTwo);
+                }
+            }
+
+            public int Multiply(int numberOne, int numberTwo)
+            {
+                return (numberOne * numberTwo);
+            }
+        }
+    }
